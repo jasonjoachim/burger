a06b83782e8a0994920dc650742af285e7a520f2
jasonjoachim <joachim.jason@gmail.com>
2017-09-20 01:52:38 -0500

    file structure, db created

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Jac');
+INSERT INTO burgers (name) VALUES ('Thirder Pounder');
+INSERT INTO burgers (name, devoured) VALUES ('DubbleDubble', true);
+INSERT INTO burgers (name, devoured) VALUES ('Royale w Cheese', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whooper', true);
+INSERT INTO burgers (name) VALUES ('Bubonic');
