6f72916249206c51647c5cf33edae85adc9861e0
jasonjoachim <joachim.jason@gmail.com>
2017-09-21 01:40:51 -0500

    routes set up, some html

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Big Jac');
-INSERT INTO burgers (name) VALUES ('Thirder Pounder');
-INSERT INTO burgers (name, devoured) VALUES ('DubbleDubble', true);
-INSERT INTO burgers (name, devoured) VALUES ('Royale w Cheese', true);
-INSERT INTO burgers (name, devoured) VALUES ('Whooper', true);
-INSERT INTO burgers (name) VALUES ('Bubonic');
+INSERT INTO burgers (burger_name) VALUES ('Big Jac');
+INSERT INTO burgers (burger_name) VALUES ('Thirder Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('DubbleDubble', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale w Cheese', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whooper', true);
+INSERT INTO burgers (burger_name) VALUES ('Bubonic');
